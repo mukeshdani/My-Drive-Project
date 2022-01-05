1530ca0260a6040c6cc709f9d1e52a67dfbece71
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-05 18:33:39 +0530

    add event listner on button

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
 (function(){
-    alert(" hello ");
+    let btnAddFolder = document.querySelector("#btnAddFolder");
+    let divContainer = document.querySelector("#divContainer");
+
+    btnAddFolder.addEventListener("click", addFolder);
+
+    function addFolder(){
+        alert("hello");
+    }
+
+
 })();
