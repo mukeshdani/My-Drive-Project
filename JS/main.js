c5cf366cec5349411e1b9638d7b703f6fa6cd10c
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-08 18:53:46 +0530

    add template in html & add js in addfolder section

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,22 @@
 (function(){
     let btnaddFolder = document.querySelector("#addFolder");
     let btnaddTextFolder = document.querySelector("#addTextFolder");
+    let divbreadcrumb = document.querySelector("#breadcrumb");
+    let divContainer = document.querySelector("#container");
+    let templates = document.querySelector("#templates");
  
     btnaddFolder.addEventListener("click" , addFolder);
     btnaddTextFolder.addEventListener("click", addTextFile);
  
     function addFolder(){
-         let fname = prompt("Enter Folder name ");
-         console.log(fname);
+        let fname = prompt("Enter folder name");
+    let divFolderTemplate = templates.content.querySelector(".folder");
+    let divFolder = document.importNode(divFolderTemplate, true);
+
+    let divName = divFolder.querySelector("[purpose=name]");
+    divName.innerHTML = fname;
+    
+    divContainer.appendChild(divFolder);
     }
  
     function addTextFile(){
