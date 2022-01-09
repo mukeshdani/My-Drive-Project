b5fd17e446b8f0224d76d8c29c2ecdb8ed26720e
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-09 16:49:10 +0530

    persist html in browser

--- JS/main.js
+++ JS/main.js
@@ -4,19 +4,29 @@
     let divbreadcrumb = document.querySelector("#breadcrumb");
     let divContainer = document.querySelector("#container");
     let templates = document.querySelector("#templates");
+
+    let resources = [];
+   let cfid = -1;
  
     btnaddFolder.addEventListener("click" , addFolder);
     btnaddTextFolder.addEventListener("click", addTextFile);
  
     function addFolder(){
-        let fname = prompt("Enter folder name");
-    let divFolderTemplate = templates.content.querySelector(".folder");
-    let divFolder = document.importNode(divFolderTemplate, true);
-
-    let divName = divFolder.querySelector("[purpose=name]");
-    divName.innerHTML = fname;
-    
-    divContainer.appendChild(divFolder);
+        let rname = prompt("Enter folder name");
+        let rid = resources.length;
+        let pid = cfid;
+  
+        
+        addFolderHTML(rname,rid , pid);
+        // persist
+        // save 
+        resources.push({
+            rid:rid,
+            rname:rname,
+            pid:cfid
+        });
+        //
+        saveToStorage(); 
     }
  
     function addTextFile(){
@@ -43,8 +53,28 @@
     function viewTextFile(){
  
     }
+    function addFolderHTML(rname, rid , pid){
+        let divFolderTemplate = templates.content.querySelector(".folder");
+        let divFolder = document.importNode(divFolderTemplate, true);
+    
+            let divName = divFolder.querySelector("[purpose='name']");
+            let spanRename = divFolder.querySelector("[action='rename']");
+            let spanDelete = divFolder.querySelector("[action='delete']");
+            let spanView = divFolder.querySelector("[action='view']");
+       
+    
+            spanRename.addEventListener("click", renameFolder);
+            spanDelete.addEventListener("click", deleatFolder);
+            spanView.addEventListener("click", viewFolder);
+               divName.innerHTML = rname;
+               divFolder.setAttribute("rid", rid);
+               divFolder.setAttribute("pid", pid);
+        
+        divContainer.appendChild(divFolder);
+       }
     function saveToStorage(){
- 
+        let rjson = JSON.stringify(resources);
+        localStorage.setItem("data", rjson);
     }
     function loadFromStorage(){
  
